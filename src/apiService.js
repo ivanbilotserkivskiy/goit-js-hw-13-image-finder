a48a9e128ee4e79517a67d16c316b3502b5ecef2
ivanbilotserkivskiy <ivanbilotserkivskiy@gmail.com>
2021-10-02 04:33:25 +0300

    das

---
+++ src/apiService.js
@@ -0,0 +1,41 @@
+// export default function onSearch(e) {
+//     e.preventDefault();
+
+//     // const searchQuery = e.currentTarget.elements.query.value
+//     const searchQuery = inputRef.value
+    
+//     const url = 
+//     `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=1&per_page=12&key=12021941-6c04822eada1d274ea5c3520e`
+
+//     fetch(url)
+//     .then(response => response.json())
+//     .then(console.log)
+//     .catch(error => console.log(error))
+// }
+export default class GalleryApiService {
+    constructor() {
+        this.searchQuery= '';
+        this.page = 1;
+    }
+    fetchGallery() {
+    
+    const url = 
+    `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=12021941-6c04822eada1d274ea5c3520e`
+
+    return fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        this.page +=1;
+        console.log(data)  
+        return data.hits    
+        
+    })
+    .catch(error => console.log(error))
+}
+get query () {
+    return this.searchQuery;
+}
+set query(newQuerry) {
+    this.searchQuery = newQuerry
+}
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
 import './sass/main.scss';
+import galleryTpl from '../src/templates/picturesTpl.hbs'
+// import onSearch from './apiService';
+import GalleryApiService from './apiService';
+
+
+
+const _ = require('lodash');
+
+
+const formRef = document.querySelector('.search-form')
+const listRef = document.querySelector('.gallery')
+const inputRef = document.querySelector('input')
+const moreBtnRef = document.querySelector('.more-btn')
+const searchBtnRef = document.querySelector('.search-btn')
+
+formRef.addEventListener('submit', onSearch)
+moreBtnRef.addEventListener('click', onLoadMore)
+
+
+function onScroll() {
+    moreBtnRef.scrollIntoView({
+        behavior: 'smooth',
+        block: 'end',
+      });
+}
+const galleryApiService = new GalleryApiService()
+
+function onSearch(e) {
+    e.preventDefault();
+
+    // const searchQuery = e.currentTarget.elements.query.value
+    // galleryApiService.query = inputRef.value
+    galleryApiService.query = e.currentTarget.elements.query.value
+
+    galleryApiService.fetchGallery().then(appendGalleryMarkup)
+}
+
+function onLoadMore() {
+    galleryApiService.fetchGallery().then(appendGalleryMarkup)
+    
+}
+
+function appendGalleryMarkup(hits) {
+    listRef.insertAdjacentHTML('beforeend', galleryTpl(hits))
+    onScroll()
+}
+
